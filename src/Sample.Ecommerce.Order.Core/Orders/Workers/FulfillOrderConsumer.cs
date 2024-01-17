b6d29a15b2d204939ea24582ca83a86f229e6af9
Andre Meira <andre._.meira@hotmail.com>
2024-01-17 16:49:34 -0400

    Ajuste nos consumers e criação de atividades para realizar pagamento e baixar produto no estoque.

---
+++ src/Sample.Ecommerce.Order.Core/Orders/Workers/FulfillOrderConsumer.cs
@@ -0,0 +1,12 @@
+﻿using MassTransit;
+using Sample.Ecommerce.Domain.Contracts.Orders;
+
+namespace Sample.Ecommerce.Order.Core.Orders.Workers;
+
+internal sealed class FulfillOrderConsumer : IConsumer<FulfillOrder>
+{
+    public Task Consume(ConsumeContext<FulfillOrder> context)
+    {
+        throw new NotImplementedException();
+    }
+}
